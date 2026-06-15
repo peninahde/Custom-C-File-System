92307695e9d96d1034d52f56b89617e7ccc10009
Avi Grad <avigrad7@gmail.com>
2026-06-15 13:51:18 +0300

    Added files from guidelines

---
+++ fs.c
@@ -0,0 +1,40 @@
+#include "fs.h"
+
+int fs_format(const char *disk_path) {
+    // TODO: Implement according to requirements
+    return 0;
+}
+
+int fs_mount(const char *disk_path) {
+    // TODO: Implement according to requirements
+    return 0;
+}
+
+void fs_unmount() {
+    // TODO: Implement according to requirements
+}
+
+int fs_create(const char *filename) {
+    // TODO: Implement according to requirements
+    return 0;
+}
+
+int fs_delete(const char *filename) {
+    // TODO: Implement according to requirements
+    return 0;
+}
+
+int fs_list(char filenames[][MAX_FILENAME], int max_files) {
+    // TODO: Implement according to requirements
+    return 0;
+}
+
+int fs_write(const char *filename, const void *data, int size) {
+    // TODO: Implement according to requirements
+    return 0;
+}
+
+int fs_read(const char *filename, void *buffer, int size) {
+    // TODO: Implement according to requirements
+    return 0;
+}
