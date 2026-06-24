94f30b545d7d110d8de2fdc3d2627d0b24ff10db
Avi Grad <avigrad7@gmail.com>
2026-06-24 13:46:54 +0300

    Made it a bit more readable

--- fs.c
+++ fs.c
@@ -10,9 +10,9 @@ int fs_format(const char *disk_path) {
     // Initialize Superblock (4KB = 1 block)
     sb.total_blocks = MAX_BLOCKS;
     sb.block_size = BLOCK_SIZE;
-    sb.free_blocks = 2550; //TODO: can we make it a non-constant?
+    sb.free_blocks = 2550; 
     sb.total_inodes = MAX_FILES;
-    sb.free_inodes = MAX_FILES; //TODO: can we make it a non-constant?
+    sb.free_inodes = MAX_FILES;
 
     // Initialize Block Bitmap (4KB = 1 block)
     for (int i = 0; i < (MAX_BLOCKS / 8); i++){
@@ -41,13 +41,13 @@ int fs_format(const char *disk_path) {
     if (disk_fd == -1) return -1; 
 
     write(disk_fd, &sb, sizeof(sb)); //write the superblock (block 0)
-    lseek(disk_fd, 1 * BLOCK_SIZE, SEEK_SET); //jump to the end of the superblock block
+    lseek(disk_fd, 1 * sb.block_size, SEEK_SET); //jump to the end of the superblock block
 
     write(disk_fd, bitmap, sizeof(bitmap)); //write the bitmap (block 1)
-    lseek(disk_fd, 2 * BLOCK_SIZE, SEEK_SET); // jump to the end of the bitmap block
+    lseek(disk_fd, 2 * sb.block_size, SEEK_SET); // jump to the end of the bitmap block
     write(disk_fd, inode_table, sizeof(inode_table)); //write the inode table (8 blocks)
 
-    lseek(disk_fd, (MAX_BLOCKS * BLOCK_SIZE) - 1, SEEK_SET); //jump to the last byte of the 10MB file
+    lseek(disk_fd, (MAX_BLOCKS * sb.block_size) - 1, SEEK_SET); //jump to the last byte of the 10MB file
     write(disk_fd, "\0",1); //write null in the last byte which secures us the full 10MB file size
 
     close(disk_fd);
@@ -84,11 +84,11 @@ void fs_unmount() {
     write(disk_fd, &sb, sizeof(sb)); //write the contents of sb to the disk
 
     // read bitmap and write it to memory
-    lseek(disk_fd, 1 * BLOCK_SIZE, SEEK_SET); //go to beginning of bitmap block
+    lseek(disk_fd, 1 * sb.block_size, SEEK_SET); //go to beginning of bitmap block
     write(disk_fd, bitmap, sizeof(bitmap)); //write the contents of bitmap to the disk
 
     // read inode table and write it to memory
-    lseek(disk_fd, 2 * BLOCK_SIZE, SEEK_SET); //go to beginning of inode_table block
+    lseek(disk_fd, 2 * sb.block_size, SEEK_SET); //go to beginning of inode_table block
     write(disk_fd, inode_table, sizeof(inode_table)); //write the contents of inode_table to the disk
     
     //close disk
@@ -122,7 +122,7 @@ int fs_create(const char *filename) {
 
     // initialize the inode with the filename and zero size
     // clear out old name and put in the new name
-    strncpy(inode_table[free_inode_index].name, filename, MAX_FILENAME); //TODO: Why using strncpy and not =?
+    strncpy(inode_table[free_inode_index].name, filename, MAX_FILENAME);
 
     //set the size to 0
     inode_table[free_inode_index].size = 0;
@@ -137,7 +137,8 @@ int fs_create(const char *filename) {
 
     // updates the superblock (decrease the free_inodes)
     sb.free_inodes--;
-
+    write_back_sb();
+    write_inode(free_inode_index, &inode_table[free_inode_index]);
     return 0;
 }
 
@@ -274,13 +275,13 @@ int fs_read(const char *filename, void *buffer, int size) {
     //read data from the files blocks into the buffer
     while (total_bytes_read < bytes_to_read) {
         //figure out which block we are in
-        int block_we_are_in = total_bytes_read / BLOCK_SIZE; // (some number / 4096) should be a number 1 to 12
-        int offset_within_block = total_bytes_read % BLOCK_SIZE; //once we are in the right block, go to the right place in it
-        int bytes_left_in_current_block = BLOCK_SIZE - offset_within_block; //number of bytes we can read within the current block
+        int block_we_are_in = total_bytes_read / sb.block_size; // (some number / 4096) should be a number 1 to 12
+        int offset_within_block = total_bytes_read % sb.block_size; //once we are in the right block, go to the right place in it
+        int bytes_left_in_current_block = sb.block_size - offset_within_block; //number of bytes we can read within the current block
         int bytes_left_to_read = bytes_to_read - total_bytes_read; //number of bytes we will have left after writing to this block
 
         //figure out if we can fit the rest of the file in here or if we'll use the rest of what is left
-        int size_to_write = (bytes_left_to_read < bytes_left_in_current_block) ? bytes_left_to_read : bytes_left_in_current_block;
+        int size_to_read = (bytes_left_to_read < bytes_left_in_current_block) ? bytes_left_to_read : bytes_left_in_current_block;
 
         //go to the block we need to write to
         int physical_block = inode_table[inode_index].blocks[block_we_are_in];
@@ -291,10 +292,10 @@ int fs_read(const char *filename, void *buffer, int size) {
         }
 
         //go to the exact right place in the block
-        lseek(disk_fd, (physical_block * BLOCK_SIZE) + offset_within_block, SEEK_SET);
+        lseek(disk_fd, (physical_block * sb.block_size) + offset_within_block, SEEK_SET);
 
         //read the size_to_write into the users buffer
-        int bytes_read = read(disk_fd, (char *)buffer + total_bytes_read, size_to_write);
+        int bytes_read = read(disk_fd, (char *)buffer + total_bytes_read, size_to_read);
 
         if (bytes_read <= 0) {
             return -3; //must be a disk error
@@ -364,6 +365,6 @@ void write_back_sb() {
 }
 
 void write_bitmap() {
-    lseek(disk_fd, BLOCK_SIZE, SEEK_SET);
+    lseek(disk_fd, sb.block_size, SEEK_SET);
     write(disk_fd, bitmap, sizeof(bitmap));
 }
